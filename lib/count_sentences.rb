c61cbc99cd42dd13369b63c08d2a4dfefe52b4e0
Miguel Nazario <mnazario35@gmail.com>
2022-03-16 11:04:07 -0400

    commit tree d241e23e359f45461a116ce569063798f47b52ff

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count_array = self.split(Regexp.union(['.', '?', '!']))
+    puts count_array
+    puts count_array.include?('')
+    count_array.filter {|element| element != ''}.length
   end
 end
